f855d726f1856431f6273d60b29e9c08584be209
Adachi Kotaro <>
2017-11-01 14:50:37 +0900

    add graph to graph

--- main.rb
+++ main.rb
@@ -23,6 +23,26 @@ y_axis4 = cost_error_x_axis.map{ |e| model.baseline_method4(e.to_f / DELIMITER) 
 cost_error_trace2 = {x: cost_error_x_axis, y: y_axis4}
 cost_error_traces = [cost_error_trace1, cost_error_trace2]
 
+# 縦軸 => 誤差率ε, 横軸 => 人の正解する確率p, people_numを変動させたグラフを重ねる
+error_possibility_model_arr = []
+30.times do |t|
+  error_possibility_model_arr << Modeling.new(1 + 5 * t, POSSIBILITY_CORRECT)
+end
+error_possibility_traces_arr = []
+error_possibility_model_arr.each do |elem|
+  error_possibility_traces_arr << {x: error_possibility_x_axis, y: error_possibility_x_axis.map{ |e| elem.baseline_method1(e.to_f / DELIMITER)}}
+end
+
+# 縦軸 => 必要な人数X(コスト), 横軸 => 誤差率ε, possibility_correctを変動させたグラフを重ねる
+cost_error_model_arr = []
+9.times do |t|
+  cost_error_model_arr << Modeling.new(PEOPELE_NUM, (55 + (5.to_f * t)) / 100)
+end
+cost_error_traces_arr = []
+cost_error_model_arr.each do |elem|
+  cost_error_traces_arr << {x: cost_error_x_axis, y: cost_error_x_axis.map{ |e| elem.baseline_method3(e.to_f / DELIMITER)}}
+end
+
 # プロットする
 pl1 = Plotly::Plot.new(data: error_possibility_traces)
 pl1.layout.title = "PEOPELE_NUM = #{PEOPELE_NUM}"
@@ -35,3 +55,13 @@ pl2.layout.title = "POSSIBILITY_CORRECT = #{POSSIBILITY_CORRECT} %"
 pl2.layout.xaxis = {title: 'relative_error (%)'}
 pl2.layout.yaxis = {title: 'people'}
 pl2.show
+
+pl3 = Plotly::Plot.new(data: error_possibility_traces_arr)
+pl3.layout.xaxis = {title: 'possibility_correct (%)'}
+pl3.layout.yaxis = {title: 'relative_error (%)'}
+pl3.show
+
+pl4 = Plotly::Plot.new(data: cost_error_traces_arr)
+pl4.layout.xaxis = {title: 'relative_error (%)'}
+pl4.layout.yaxis = {title: 'people'}
+pl4.show
