662879956878d4a871ff5df5654ee0cfb427f3a1
k33asby <k33asby@gmail.com>
2017-11-23 16:00:28 +0900

    Finish python version

--- py/modeling.py
+++ py/modeling.py
@@ -1,24 +1,34 @@
 class Modeling:
 
-    def __init__(self, people_num, possibility_correct, lambda_poisson, repeat, weight):
-        self.people_num = people_num
-        self.people_half = self.half_num(people_num)
-        self.possibility_correct = possibility_correct
+    def __init__(self,lambda_poisson, repeat, weight):
         self.lambda_poisson = lambda_poisson
-        self.correcting_deadline = lambda_poisson * 2
+        self.collecting_deadline = lambda_poisson * 2
         self.repeat = repeat
-        self.poisson = np.random.poisson(lambda_poisson, 1000000)
+        self.poisson = np.random.poisson(lambda_poisson, 1000)
         self.weight = float(weight)
 
     def half_num(self,num):
-        return math.ceil(float(num) / 2)
+        return int(math.ceil(float(num) / 2))
 
     def simulate_when_people_come_list(self, people_num):
-        when_people_come = [0] * self.correcting_deadline
+        when_people_come = [0] * self.collecting_deadline
         for t in range(people_num):
             when_people_come[t] = 1
         random.shuffle(when_people_come)
         return when_people_come
+
+    def relative_error_by_majority_vote(self, people_num, possibility_correct):
+        half_num = self.half_num(people_num)
+        relative_error = 0
+        for t in range(half_num):
+            relative_error += (possibility_correct**t) * ((1 - possibility_correct)**(people_num - t)) * scm.comb(people_num, t)
+        return relative_error
+
+    def relative_error_array_by_half_opinion(self, finish_num, possibility_correct):
+        relative_error = []
+        for t in range(finish_num):
+            relative_error.append(((1 - possibility_correct)**finish_num) * (possibility_correct**t) * scm.comb(finish_num - 1 + t, t))
+        return relative_error
 
     def deciding_by_first_person_with_poisson(self, possibility_correct):
         temp_method_utility = 0
@@ -27,17 +37,69 @@ class Modeling:
             when_people_come = self.simulate_when_people_come_list(people_num)
             method_utility = 0
             for i in range(len(when_people_come)):
-                if when_people_come[i] == 0:
-                    continue
-                else:
-                    method_utility += possibility_correct - self.weight * (float(i) / self.correcting_deadline)
+                if when_people_come[i] == 0: continue
+                method_utility += possibility_correct - self.weight * (float(i) / self.collecting_deadline)
+                break
+            temp_method_utility += method_utility
+        average_method_utility = temp_method_utility / self.repeat
+        return average_method_utility
+
+    def deciding_by_majority_vote_with_poisson(self, possibility_correct, majority_vote_people):
+        temp_method_utility = 0
+        for n in range(self.repeat):
+            people_num = self.poisson[n]
+            when_people_come = self.simulate_when_people_come_list(people_num)
+            method_utility = 0
+            people_count = 0
+            if people_num < majority_vote_people: continue
+            for i in range(len(when_people_come)):
+                if when_people_come[i] == 0: continue
+                people_count += 1
+                if people_count == majority_vote_people:
+                    method_utility += (1 - self.relative_error_by_majority_vote(people_count, possibility_correct)) - self.weight * (float(i) / self.collecting_deadline)
                     break
             temp_method_utility += method_utility
         average_method_utility = temp_method_utility / self.repeat
         return average_method_utility
 
-    def test(self):
-            s = np.random.poisson(50, 10000000)
-            print(s[9000000])
-            count, bins, ignored = plt.hist(s, 14, normed=True)
-            plt.show()
+    def deciding_by_half_opinion_with_poisson(self, possibility_correct, temp_people_num):
+        temp_method_utility = 0
+        for n in range(self.repeat):
+            people_num = self.poisson[n]
+            when_people_come = self.simulate_when_people_come_list(people_num)
+            method_utility = 0
+            people_count = 0
+            people_count_arr = []
+            if people_num < temp_people_num: continue
+            for i in range(len(when_people_come)):
+                if when_people_come[i] == 0: continue
+                people_count_arr.append(i)
+                people_count += 1
+                if people_count == temp_people_num:
+                    relative_error_array = self.relative_error_array_by_half_opinion(self.half_num(temp_people_num), possibility_correct)
+                    expected_error = sum(relative_error_array)
+                    average_index = 0
+                    for index in range(len(relative_error_array)):
+                        average_index += people_count_arr[self.half_num(temp_people_num) - 1 + index] * (relative_error_array[index] / expected_error)
+                    method_utility = (1 - expected_error) - self.weight * (float(average_index) / self.collecting_deadline)
+                    break
+            temp_method_utility += method_utility
+        average_method_utility = temp_method_utility / self.repeat
+        return average_method_utility
+
+    def deciding_by_time_limit_with_poisson(self, possibility_correct, time_limit):
+        temp_method_utility = 0
+        for n in range(self.repeat):
+            people_num = self.poisson[n]
+            when_people_come = self.simulate_when_people_come_list(people_num)
+            method_utility = 0
+            people_count = 0
+            for i in range(len(when_people_come)):
+                if i >= time_limit and people_count >= 1:
+                    method_utility = (1 - self.relative_error_by_majority_vote(people_count, possibility_correct)) - self.weight * (float(i) / self.collecting_deadline)
+                    break
+                if when_people_come[i] == 0: continue
+                people_count += 1
+            temp_method_utility += method_utility
+        average_method_utility = temp_method_utility / self.repeat
+        return average_method_utility
