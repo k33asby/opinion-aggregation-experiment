e9eda27489fbc4fc2ba450ec56c296a30035c898
k33asby <k33asby@gmail.com>
2017-11-30 13:56:13 +0900

    Add new graph

--- py/PlotGraph.py
+++ py/PlotGraph.py
@@ -1,7 +1,43 @@
 class PlotGraph:
 
+    def __init__(self, lambda_poisson, repeat, weight, people_num, possibility_correct):
+        self.model = Modeling(50, 50, 0.5)
+        self.people_num = people_num
+        self.possibility_correct = possibility_correct
+
     def test(self):
-        x = np.linspace(50, 100, 1)
+        x = np.linspace(50, 100, 50)
         y = 2 * (x**2)
         plt.plot(x,y)
         plt.show()
+
+    def plot_error_possibility_graph(self):
+        x_axis = np.linspace(50,100,50)
+        y_axis_majority_vote = self.model.relative_error_by_majority_vote(self.people_num, x_axis / 100)
+        y_axis_half_opinion = sum(self.model.relative_error_array_by_half_opinion(self.model.half_num(self.people_num), x_axis / 100))
+        plt.hold(True)
+        plt.title('error_possibility_graph %s people' % self.people_num)
+        plt.xlabel('Possibility of correct')
+        plt.ylabel('Relative error')
+        plt.plot(x_axis, y_axis_majority_vote, label='Majority vote')
+        plt.plot(x_axis, y_axis_half_opinion, label='Half opinion')
+        plt.show()
+
+    def plot_cost_error_graph(self):
+        pass
+
+    def plot_error_possibility_graph_by_people_num(self):
+        x_axis = np.linspace(50,100,50)
+        y_axis_arr = []
+        for t in range(30):
+            y_axis_arr.append(self.model.relative_error_by_majority_vote(1 + 2 * t, x_axis / 100))
+        plt.hold(True)
+        plt.title('Error-possibility graph by people number')
+        plt.xlabel('Possibility of correct')
+        plt.ylabel('Relative error')
+        for y_axis in y_axis_arr:
+            plt.plot(x_axis, y_axis)
+        plt.show()
+
+    def plot_cost_error_graph_by_possibility(self):
+        pass
