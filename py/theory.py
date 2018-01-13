a85634723e6d5c7351fdb3442b339d3649d74fa9
k33asby <k33asby@gmail.com>
2018-01-13 18:15:12 +0900

    Complete decimal_for_only_poisson_and_gamma_and_g

--- py/theory.py
+++ py/theory.py
@@ -3,18 +3,18 @@
 # 単位時間あたりの到着数 λ
 # 時刻tまでにn人が到着する確率
 def poisson_probability(n, t, lambda_poisson):
-    return Decimal(math.e)**(Decimal(-1)*lambda_poisson * Decimal(t)) * (lambda_poisson * Decimal(t))**Decimal(n) / Decimal(math.factorial(Decimal(n)))
+    return float(math.e**(-lambda_poisson * t) * (lambda_poisson * t)**n / math.factorial(n))
 
 # Gamma分布
 # 時刻tにn人目が到着する確率
 def gamma_probability(n, t, lambda_poisson):
-    return lambda_poisson**Decimal(n) * Decimal(t)**(Decimal(n) - Decimal(1)) * Decimal(math.e)**(Decimal(-1)*lambda_poisson * Decimal(t)) / Decimal(math.factorial(Decimal(n) - Decimal(1)))
+    return float(lambda_poisson**n * Decimal(t)**(n - 1) * Decimal(math.e)**(-lambda_poisson * Decimal(t)) / math.factorial(n - 1))
 
 
 # 時刻Tまでにn人現われるときに， 時刻t(=< T)にm(=< n)人目が到着する確率g(m,n,t,T)
 def g(m, n, t, T, lambda_poisson):
-    return (Decimal(lambda_poisson)**Decimal(n) / (Decimal(math.factorial(Decimal(m) - Decimal(1))) * Decimal(math.factorial(Decimal(n) - Decimal(m))))) \
-    * Decimal(t)**(Decimal(m) - Decimal(1)) * (Decimal(T) - Decimal(t))**(Decimal(n) - Decimal(m)) * Decimal(math.e)**(Decimal(-1) * Decimal(lambda_poisson) * Decimal(T))
+    return float(lambda_poisson**Decimal(n) / (math.factorial(Decimal(m) - Decimal(1)) * math.factorial(Decimal(n) - Decimal(m))) \
+    * Decimal(t)**Decimal(m - 1) * Decimal(T - t)**Decimal(n - m) * Decimal(math.e)**(Decimal(-1) * Decimal(lambda_poisson) * Decimal(T)))
 
 # 多数決による判定精度
 # n人で多数決を行う場合の判定精度をacc(n)で表す
@@ -24,7 +24,7 @@ def acc_odd(n, p): # nが奇数2i-1のとき
     acc = 0
     for j in range(i):
         acc += scm.comb(2 * i - 1, j) * p**(2 * i - 1 - j) * (1 - p)**j
-    return Decimal(acc)
+    return acc
 
 def acc_even(n, p): # nが偶数2iのとき
     i = int(n / 2)
@@ -32,7 +32,7 @@ def acc_even(n, p): # nが偶数2iのとき
     for j in range(i):
         acc += scm.comb(2 * i, j) * p**(2 * i - j) * (1 - p)**j
     acc += scm.comb(2 * i, i) * p**i * (1 - p)**i / 2
-    return Decimal(acc)
+    return acc
 
 def acc(n, p):
     if n % 2 == 1:
@@ -46,7 +46,7 @@ def acc(n, p):
 # 効用を予測精度と所要時間の差で表す
 def time_priority_method(t, w, p, lambda_poisson):
     if t == 0: return 0
-    return np.sum(poisson_probability(i, t, lambda_poisson) * acc(i, p) for i in xrange(1,200)) - (w * Decimal(t))
+    return np.sum(poisson_probability(i, t, lambda_poisson) * acc(i, p) for i in xrange(1,200)) - (w * t)
 
 
 # 増減を調べる
@@ -60,7 +60,7 @@ def inc_and_dec_time_priority_method(w, p, lambda_poisson):
 # 効用を予測精度と所要時間の差で表す
 def poll_priority_method(n, w, p, lambda_poisson):
     if n == 0: return 0
-    return acc(n, p) - Decimal(integrate.quad(lambda t: w *Decimal(t) * gamma_probability(n, t, lambda_poisson), Decimal(0), Decimal(1000))[0])
+    return acc(n, p) - integrate.quad(lambda t: w * t * gamma_probability(n, t, lambda_poisson), 0, 1000)[0]
 
 
 # 増減を調べる
@@ -74,9 +74,12 @@ def inc_and_dec_poll_priority_method(w, p, lambda_poisson):
 # 効用を予測精度と所要時間の差で表す
 def vote_priority_method(k, w, p, lambda_poisson):
     if k == 0: return 0
-    value = integrate.quad(lambda t: w * Decimal(t) * gamma_probability(j, t, lambda_poisson), 0, 1000)[0]
-    return np.sum((scm.comb(j - 1, j - k) * p**(k - 1) * (1 - p)**(j - k) * p * (1 - value)) + (scm.comb(j - 1, j - k) * p**(j - k) * (1 - p)**(k - 1) * (1 - p) * -value ) for j in xrange(k, 2 * k))
-
+    utility = 0
+    for j in xrange(k, 2 * k):
+        value = integrate.quad(lambda t: w * t * gamma_probability(j, t, lambda_poisson), 0, 1000)[0]
+        utility += (scm.comb(j - 1, j - k) * p**(k - 1) * (1 - p)**(j - k) * p * (1 - value)) + (scm.comb(j - 1, j - k) * p**(j - k) * (1 - p)**(k - 1) * (1 - p) * -value )
+    return utility
+    
 # 増減を調べる
 def inc_and_dec_vote_priority_method(w, p, lambda_poisson):
     for k in xrange(1, 1000):
